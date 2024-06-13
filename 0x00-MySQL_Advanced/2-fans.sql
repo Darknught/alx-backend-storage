e8e28d3e3f21bf74a6f255affbf4a42f4e6122ce
Darknught <hassino66@yahoo.com>
2024-06-13 09:11:00 +0000

    code

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,20 +1,12 @@
 -- A SQL script that ranks country origins of bands, ordered by the number
 -- of non-unique fans. Column must be origin and nb_fans
 -- Create a view to rank country origins by the number of fans
-CREATE VIEW country_rankings AS
 SELECT
-    origin,
-    SUM(nb_fans) AS total_fans
+  origin,
+  COUNT(*) AS nb_fans
 FROM
-    bands
+  bands
 GROUP BY
-    origin
+  origin
 ORDER BY
-    total_fans DESC;
-
--- Select from the view to get the rankings
-SELECT
-    origin,
-    total_fans
-FROM
-    country_rankings;
+  nb_fans DESC;
