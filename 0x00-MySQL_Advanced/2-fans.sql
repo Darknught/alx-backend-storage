e69c993fc25cbbfd9a8325036d3d71bd0cc356bd
Darknught <hassino66@yahoo.com>
2024-06-13 22:14:22 +0000

    code

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,14 +1,8 @@
 -- A SQL script that ranks country origins of bands, ordered by the number
 -- of non-unique fans. Column must be origin and nb_fans
 -- Create a view to rank country origins by the number of fans
-USE holberton;
-
-SELECT
-  origin,
-  SUM(fans) AS nb_fans
-FROM
-  metal_bands
-GROUP BY
-  origin
-ORDER BY
-  nb_fans DESC;
+-- rank country origins of bands.
+SELECT origin, SUM(fans) AS nb_fans
+    FROM metal_bands
+    GROUP BY origin
+    ORDER BY nb_fans DESC;
