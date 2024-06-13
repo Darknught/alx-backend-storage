0cd3caa60385a1edb4a8e1a53422b7360393ec4e
Darknught <hassino66@yahoo.com>
2024-06-13 06:35:48 +0000

    code

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,6 +1,6 @@
 -- SQL script that creates a table users with attributes id, email, name
 CREATE TABLE IF NOT EXISTS users (
-	id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
-	email STRING VARCHAR(255) UNIQUE NOT NULL,
+	id INT AUTO_INCREMENT PRIMARY,
+	email STRING VARCHAR(255) NOT NULL UNIQUE,
 	name STRING VARCHAR(255)
 );
