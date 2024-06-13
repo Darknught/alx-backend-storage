885efd52de7ffc5f3b0d9becf0a7fa10197bb4c5
Darknught <hassino66@yahoo.com>
2024-06-13 17:12:33 +0000

    code

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,6 +1,8 @@
 -- A SQL script that ranks country origins of bands, ordered by the number
 -- of non-unique fans. Column must be origin and nb_fans
 -- Create a view to rank country origins by the number of fans
+USE holberton;
+
 SELECT
   origin,
   COUNT(*) AS nb_fans
