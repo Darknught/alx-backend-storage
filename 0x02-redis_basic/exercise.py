dfdd3b7f3469b21bb04eaf7088a2b15cf921c96a
Darknught <hassino66@yahoo.com>
2024-06-19 14:18:47 +0000

    code

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,7 +2,7 @@
 """ A Cache class that store an instance of the Redis client."""
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable, Optional
 
 
 class Cache:
@@ -25,3 +25,51 @@ class Cache:
         else:
             self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Optional[Callable[[bytes], Union[str, int, float]]] = None) -> Optional[Union[str, int, float]]:
+        """
+        Retrieves the data stored under the given key from the Redis cache.
+
+        Args:
+            key (str): The key of the data to be retrieved.
+            fn (Optional[Callable[[bytes], Union[str, int, float]]]):
+            An optional function to convert the retrieved data to the desired format.
+
+        Returns:
+            Optional[Union[str, int, float]]: The retrieved data, converted
+            using the provided function if specified,
+            or None if the key does not exist.
+        """
+        value = self._redis.get(key)
+        if value is None:
+            return None
+        if fn is None:
+            return value.decode()
+        else:
+            return fn(value)
+
+    def get_str(self, key: str) -> Optional[str]:
+        """
+        Retrieves the data stored under the given key as a string.
+
+        Args:
+            key (str): The key of the data to be retrieved.
+
+        Returns:
+            Optional[str]: The retrieved data as a string,
+            or None if the key does not exist.
+        """
+        return self.get(key, lambda x: x.decode())
+
+    def get_int(self, key: str) -> Optional[int]:
+        """
+        Retrieves the data stored under the given key as an integer.
+
+        Args:
+            key (str): The key of the data to be retrieved.
+
+        Returns:
+            Optional[int]: The retrieved data as an integer,
+            or None if the key does not exist.
+        """
+        return self.get(key, lambda x: int(x.decode()))
