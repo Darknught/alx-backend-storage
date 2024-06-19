d795256b24b6fff07e8f2d0b3e388cc271ce2172
Darknught <hassino66@yahoo.com>
2024-06-19 13:43:54 +0000

    code

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+""" A Cache class that store an instance of the Redis client."""
+import redis
+import uuid
+from typing import Union
+
+
+class Cache:
+    def __init__(self):
+        """ Init method creates a new Redis client instance using redis.Redis()
+        and stores it as a private variable and clears the entire Redis databas
+        using flushdb()
+        """
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        """ Method takes a data arg and returns a string.
+        It generates a random key using uuid and store the input data in Redis
+        using the random key and return the key.
+        """
+        key = str(uuid.uuid4())
+        self._redis.set(key, str(data).encode())
+        return key
