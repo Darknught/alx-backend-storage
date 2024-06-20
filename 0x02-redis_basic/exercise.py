869ea926e83cc5e288c9cb060c201a3b53135b5f
Darknught <hassino66@yahoo.com>
2024-06-20 16:31:59 +0000

    code

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -96,7 +96,8 @@ class Cache:
             try:
                 return fn(value)
             except Exception:
-                raise ValueError("Error converting value using the provided callable")
+                raise ValueError(
+                        "Error converting value using the provided callable")
 
     def get_str(self, key: str) -> Optional[str]:
         """
